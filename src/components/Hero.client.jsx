408b18629fef0efce8136563c042c3e78f032ec4
yuyechen-toronto <93956026+yuyechen-toronto@users.noreply.github.com>
2023-01-26 20:44:18 -0500

    Update Hero.client.jsx

--- src/components/Hero.client.jsx
+++ src/components/Hero.client.jsx
@@ -1,6 +1,6 @@
 import snowboard from '~/assets/snowboard-mobile.jpg';
 import {Image} from '@shopify/hydrogen';
 
-export default function Hero() {
+export default function HeroImage() {
   return <Image src={snowboard} width="100" height="100" />;
 }
