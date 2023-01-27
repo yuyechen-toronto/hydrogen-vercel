1fa6ab3d3e8fa52272c998f211aac97e349de36a
yuyechen-toronto <93956026+yuyechen-toronto@users.noreply.github.com>
2023-01-26 20:49:58 -0500

    Delete HeroImage.tsx

--- src/components/HeroImage.tsx
+++
@@ -1,6 +0,0 @@
-import snowboard from '~/assets/snowboard-mobile.jpg';
-import {Image} from '@shopify/hydrogen';
-
-export default function HeroImage() {
-  return <Image src={snowboard} width="100" height="100" />;
-}
